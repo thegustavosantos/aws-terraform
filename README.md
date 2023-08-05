e9f4c8844e02dc512bb6abdd1e129073ae944032
Gusta <thegustavosantos@outlook.com>
2023-08-05 00:12:14 -0300

    include sg, s3 and depends

--- README.md
+++ README.md
@@ -2,14 +2,19 @@
     AWS templates using terraform:
         - EC2 simple example
 
-# Anotations
+# anotations
     Links:
         - https://www.terraform.io/
+        - https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources
+        - https://registry.terraform.io/providers/hashicorp/http/latest/docs/data-sources/http
+
 
     Comandos:
         - terraform init    : inicializa a cria a pasta .terraform
         - terraform plan    : mostrar os recursos que serão criados e suas características
         - terraform apply   : realiza a aplicação do recurso conforme especificação do arquivo
+        - terraform show    : mostra um foto de como está o ambiente
 
     Dicas:
-        Sequência de comandos para deploy: init, plan, apply
+        - Sequência de comandos para deploy : init, plan, apply
+        - Terraform.tfstatus                : dados da última versão do recurso, quando rodamos o show é de lá que puxa os dados.
--- ec2/lab1/main.tf
+++ ec2/lab1/main.tf
@@ -14,11 +14,62 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-    count = 3
-    ami = "ami-0f34c5ae932e6f0e4"
+    count         = 3
+    ami           = "ami-0f34c5ae932e6f0e4" # AWS Linux
     instance_type = "t2.micro"
-    key_name = "terraform-aws"
+    key_name      = "terraform-aws"
     tags = {
-      Name = "dev-${count.index}"
+      Name        = "dev-${count.index}"
     }
+    vpc_security_group_ids = ["${aws_security_group.access-ssh.id}"]
+}
+
+resource "aws_security_group" "access-ssh" {
+  name        = "access-ssh"
+  description = "SSH Access SG"
+
+  ingress {
+    description      = "ingress ssh rule"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
+  tags = {
+    Name = "ssh-access"
+  }
+}
+
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+resource "aws_s3_bucket" "dev-4" {
+  bucket  = "lab1-dev-4"
+
+  tags = {
+    Name        = "lab1-dev-4"
+  }
+}
+
+resource "aws_instance" "dev-4" {
+    ami           = "ami-053b0d53c279acc90" #Ubuntu
+    instance_type = "t2.micro"
+    key_name      = "terraform-aws"
+    tags = {
+      Name        = "dev-4"
+    }
+    vpc_security_group_ids = ["${aws_security_group.access-ssh.id}"]
+    depends_on = [aws_s3_bucket.dev-4]
+}
+
+resource "aws_instance" "dev-5" {
+    ami           = "ami-053b0d53c279acc90" #Ubuntu
+    instance_type = "t2.micro"
+    key_name      = "terraform-aws"
+    tags = {
+      Name        = "dev-5"
+    }
+    vpc_security_group_ids = ["${aws_security_group.access-ssh.id}"]
 }
